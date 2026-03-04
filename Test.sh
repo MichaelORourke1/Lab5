da61b0916887ee230fde7cefd6ee0c2f83173f0b
MichaelORourke1 <MOROUR301@caledonian.ac.uk>
2026-03-04 16:57:34 +0000

    Test bash

---
+++ Test.sh
@@ -0,0 +1,11 @@
+echo "Testing integer"
+python3 Dec2Hex.py 5
+echo
+echo "Testing float"
+python3 Dec2Hex.py 5.6789
+echo
+echo "Testing string"
+python3 Dec2Hex.py "Hi"
+echo
+echo "Testing no input"
+echo Dec2Hex.py
