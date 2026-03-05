b10b0de5b4855687063ea33f0bcf3e52f0ed9c7c
MichaelORourke1 <MOROUR301@caledonian.ac.uk>
2026-03-05 11:27:15 +0000

    blah

--- Hello.py
+++ Hello.py
@@ -1,9 +1,1 @@
-# This is a simple Python program
-
-# Your program begins with a call to main()
-# Prints "Hello, World" to the terminal window.
-def main():
-    print("Hello, World")
-
-if __name__ == "__main__":
-    main()
+print("Hello World")
